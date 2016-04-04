f39667215b16dab3cda8e834f8adb789a8413c99
monsterLin <876948462@qq.com>
2016-04-04 16:26:50 +0800

    Add Bmob

---
+++ app/src/main/java/com/monsterlin/blives/constants/BmobKey.java
@@ -0,0 +1,14 @@
+package com.monsterlin.blives.constants;
+
+/**
+ * Bmob 的相关常量
+ * Created by monsterLin on 2016/4/4.
+ */
+public class BmobKey {
+
+    /**
+     * Bmob的APPkey
+     */
+    public  static final String APPKEY = "543334e06f481a8e6a4b8c2933dcd8f0" ;
+
+}
