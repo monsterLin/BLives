9fb6c2c0d7313e0ceb80072838a8262f1a2f061c
monsterLin <876948462@qq.com>
2016-04-12 15:53:53 +0800

    add youmeng sdk

---
+++ app/src/main/java/com/monsterlin/blives/application/BApplication.java
@@ -0,0 +1,10 @@
+package com.monsterlin.blives.application;
+
+import android.app.Application;
+
+/**
+ * Created by monsterLin on 2016/4/12.
+ */
+public class BApplication  extends Application{
+
+}
