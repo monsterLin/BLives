e5a266872eaaee258102ff3f789c19531d20dc4e
monsterLin <876948462@qq.com>
2016-04-14 15:51:47 +0800

    repair SceneryAdapter and SceneryFragment

--- app/src/main/java/com/monsterlin/blives/adapter/SceneryAdapter.java
+++ app/src/main/java/com/monsterlin/blives/adapter/SceneryAdapter.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.monsterlin.blives.R;
@@ -17,63 +18,151 @@ import java.util.List;
 import cn.bmob.v3.datatype.BmobFile;
 
 /**
- * 图片适配器
- * Created by monsterLin on 2016/4/7.
+ * Created by monsterLin on 2016/4/14.
  */
-public class SceneryAdapter extends RecyclerView.Adapter<SceneryAdapter.ItemView> {
+public class SceneryAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
+
+    private static final int TYPE_ITEM = 0;
+    private static final int TYPE_FOOTER = 1;
+
 
     private List<Scenery> mList ;
     private Context mContext ;
-    private LayoutInflater mInflater;
+
 
     public SceneryAdapter(List<Scenery> mList, Context mContext) {
         this.mList = mList;
         this.mContext = mContext;
-        mInflater=LayoutInflater.from(mContext);
     }
 
-    @Override
-    public ItemView onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view=mInflater.inflate(R.layout.item_scenery,parent,false);
-        ItemView viewHolder=new ItemView(view);
-        return viewHolder;
+    /**
+     * 声明一个接口，用于实现点击事件
+     */
+    public interface  OnItemClickListener{
+        void OnItemClick(int position, View view);
+        void OnItemLongClick(int position, View view);
     }
 
-    @Override
-    public void onBindViewHolder(ItemView holder, int position) {
+    private OnItemClickListener mOnItemClickListener;
 
-        WindowManager wm = (WindowManager) mContext
-                .getSystemService(Context.WINDOW_SERVICE);
-
-        int width = wm.getDefaultDisplay().getWidth();
-
-        BmobFile img = mList.get(position).getSceneryimg();
-        // img.loadImage(mContext,holder.iv_img,width/2,200);
-        img.loadImageThumbnail(mContext,holder.iv_img,width/2,200);
-        holder.tv_intro.setText(mList.get(position).getSceneryintro());
-
-
+    public  void setOnItemClickListener(OnItemClickListener listener){
+        this.mOnItemClickListener=listener;
     }
+
+
+    // RecyclerView的count设置为数据总条数+ 1（footerView）
 
     @Override
     public int getItemCount() {
-        return mList.size();
+        return mList.size()+1;
     }
 
 
-    class ItemView extends RecyclerView.ViewHolder{
+    @Override
+    public int getItemViewType(int position) {
+        if (position + 1 == getItemCount()) {
+            return TYPE_FOOTER;
+        } else {
+            return TYPE_ITEM;
+        }
+    }
 
-        ImageView iv_img ;
-        TextView tv_intro;
 
-        public ItemView(View itemView) {
-            super(itemView);
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        if (viewType == TYPE_ITEM) {
+            View view = LayoutInflater.from(mContext).inflate(R.layout.item_scenery, parent,
+                    false);
+            return new ItemViewHolder(view);
+        } else if (viewType == TYPE_FOOTER) {
+            if(mList.size()!=0){
+                View view = LayoutInflater.from(mContext).inflate(R.layout.item_foot, parent,
+                        false);
+                return new FootViewHolder(view);
+            }else {
+                View view = LayoutInflater.from(mContext).inflate(R.layout.item_foot, parent,
+                        false);
+                TextView tv =(TextView)view.findViewById(R.id.tv_foot);
+                ProgressBar progressBar = (ProgressBar) view.findViewById(R.id.progressBar);
+                progressBar.setVisibility(View.INVISIBLE);
+                tv.setText("已到最底部");
+                return new FootViewHolder(view);
+            }
 
-            iv_img= (ImageView) itemView.findViewById(R.id.iv_img);
-            tv_intro= (TextView) itemView.findViewById(R.id.tv_intro);
+        }
+        return null;
+    }
+
+
+    @Override
+    public void onBindViewHolder(final RecyclerView.ViewHolder holder, int position) {
+        if (holder instanceof ItemViewHolder) {
+
+            if (mList.get(position).getSceneryimg()!=null){
+                WindowManager wm = (WindowManager) mContext
+                        .getSystemService(Context.WINDOW_SERVICE);
+
+                int width = wm.getDefaultDisplay().getWidth();
+
+                BmobFile img = mList.get(position).getSceneryimg();
+                img.loadImageThumbnail(mContext,  ((ItemViewHolder) holder).iv_img,width/2,200);
+            }else {
+                ((ItemViewHolder) holder).iv_img.setImageResource(R.drawable.ic_nopic);
+            }
+
+            ((ItemViewHolder) holder).tv_intro.setText(mList.get(position).getSceneryintro());
+
+
+            if (mOnItemClickListener!=null){
+
+                holder.itemView.setOnClickListener(new View.OnClickListener() {
+
+                    @Override
+                    public void onClick(View v) {
+                        int LayoutPosition=holder.getLayoutPosition(); //得到布局的position
+                        mOnItemClickListener.OnItemClick(LayoutPosition,holder.itemView);
+
+                    }
+                });
+
+                //longclickListener
+                holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+                    @Override
+                    public boolean onLongClick(View v) {
+                        int LayoutPosition=holder.getLayoutPosition(); //得到布局的position
+                        mOnItemClickListener.OnItemLongClick(LayoutPosition,holder.itemView);
+                        return false;
+                    }
+                });
+            }
         }
     }
 
 
 
+
+
+
+
+
+    static class ItemViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView iv_img ;
+        TextView tv_intro;
+
+        public ItemViewHolder(View itemView) {
+            super(itemView);
+
+            iv_img= (ImageView) itemView.findViewById(R.id.iv_img);
+            tv_intro= (TextView) itemView.findViewById(R.id.tv_intro);
+        }
+
+    }
+
+    static class FootViewHolder extends RecyclerView.ViewHolder {
+
+        public FootViewHolder(View view) {
+            super(view);
+        }
+    }
 }
--- app/src/main/java/com/monsterlin/blives/navfragment/SceneryFragment.java
+++ app/src/main/java/com/monsterlin/blives/navfragment/SceneryFragment.java
@@ -12,13 +12,13 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.monsterlin.blives.R;
 import com.monsterlin.blives.adapter.SceneryAdapter;
 import com.monsterlin.blives.entity.Scenery;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import cn.bmob.v3.BmobQuery;
@@ -30,14 +30,19 @@ import cn.bmob.v3.listener.FindListener;
  */
 public class SceneryFragment extends Fragment {
 
-    private RecyclerView ryimgs;
-    private Context mContext;
-    private SceneryAdapter adapter;
+    private Context mContext ;
+    private SwipeRefreshLayout srl ;
+    private RecyclerView ryimgs ;
 
-    BmobQuery<Scenery> query;
-    private SwipeRefreshLayout srl;
-    //private List<Scenery> mList = new ArrayList<>();
-    private ProgressBar progressBar;
+    BmobQuery<Scenery> query ;
+    private List<Scenery> mList = new ArrayList<>();
+    private SceneryAdapter adapter ;
+
+    boolean isLoading ; //监听加载状态
+
+    private int limit =10;		// 每页的数据是8条
+    private int curPage = 0;		// 当前页的编号，从0开始
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_scenery, container, false);
@@ -51,45 +56,26 @@ public class SceneryFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         initView(view);
         initData();
-        progressBar.setVisibility(View.INVISIBLE);
     }
 
-
-    private void initView(View view) {
-        ryimgs= (RecyclerView) view.findViewById(R.id.ryimgs);
-        progressBar= (ProgressBar) view.findViewById(R.id.progressBar);
-        //srl= (SwipeRefreshLayout) view.findViewById(R.id.srl);
-
-//
-//        srl.setColorSchemeResources(android.R.color.holo_blue_light, android.R.color.holo_red_light, android.R.color.holo_orange_light, android.R.color.holo_green_light);
-//        srl.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-//            @Override
-//            public void onRefresh() {
-//                initData();
-//                srl.setRefreshing(false);
-//            }
-//        });
-//
-//
-    }
-
-
-
-
+    /**
+     * 初始化数据
+     */
     private void initData() {
+
         query= new BmobQuery<Scenery>();
         query.order("-updatedAt");
-        query.setLimit(10);
+        query.setLimit(limit);
+        query.setSkip(curPage*limit);
+        curPage++;
         query.findObjects(mContext, new FindListener<Scenery>() {
             @Override
             public void onSuccess(List<Scenery> list) {
                 //　新数据的添加
-                final LinearLayoutManager layoutManager = new LinearLayoutManager(mContext);
-                ryimgs.setLayoutManager(layoutManager);
-                adapter = new SceneryAdapter(list,mContext);
-                ryimgs.setAdapter(adapter);
-                //ryimgs.setLayoutManager(new StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL));
-                ryimgs.setLayoutManager(new GridLayoutManager(mContext,2));
+                mList.addAll(list);
+                if (null != adapter){
+                    adapter.notifyDataSetChanged();
+                }
             }
 
             @Override
@@ -99,6 +85,102 @@ public class SceneryFragment extends Fragment {
         });
 
     }
+
+
+    private void getData(int page) {
+
+        // 进行服务器端的分页处理
+        BmobQuery<Scenery> query = new BmobQuery<>();
+        query.order("-updatedAt");
+        query.setSkip(page*limit+1);
+        query.setLimit(5);
+        curPage++;
+        query.findObjects(mContext, new FindListener<Scenery>() {
+            @Override
+            public void onSuccess(List<Scenery> list) {
+                if(list.size()!=0){
+                    mList.addAll(list);
+                    adapter.notifyDataSetChanged();
+                }
+
+
+            }
+
+            @Override
+            public void onError(int i, String s) {
+
+            }
+        });
+
+
+
+    }
+
+
+
+    private void initView(View view) {
+        ryimgs= (RecyclerView) view.findViewById(R.id.ryimgs);
+        srl= (SwipeRefreshLayout) view.findViewById(R.id.srl);
+
+        srl.setColorSchemeResources(android.R.color.holo_blue_light, android.R.color.holo_red_light, android.R.color.holo_orange_light, android.R.color.holo_green_light);
+        srl.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                initData();
+                srl.setRefreshing(false);
+            }
+        });
+
+
+        final LinearLayoutManager layoutManager = new LinearLayoutManager(mContext);
+        ryimgs.setLayoutManager(new GridLayoutManager(mContext,2));
+        adapter = new SceneryAdapter(mList,mContext);
+        ryimgs.setAdapter(adapter);
+
+        //滑动监听
+        ryimgs.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+
+                /**
+                 * 当RecyclerView的滑动状态改变时触发
+                 * 0： 手指离开屏幕
+                 * 1：  手指触摸屏幕
+                 * 2： 手指加速滑动并放开，此时滑动状态伴随SCROLL_STATE_IDLE
+                 */
+
+            }
+
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+
+                int lastVisibleItemPosition =layoutManager.findLastVisibleItemPosition(); //最后一个可视的Item
+                if (lastVisibleItemPosition + 1 == adapter.getItemCount()) {
+
+                    boolean isRefreshing = srl.isRefreshing();  //刷新状态
+
+                    if (isRefreshing) {
+                        adapter.notifyItemRemoved(adapter.getItemCount());
+                        return;
+                    }
+                    if (!isLoading) {
+                        isLoading = true;
+                        //　加载数据
+                        getData(curPage);
+
+                        isLoading = false;
+                    }
+                }
+
+            }
+        });
+    }
+
+
+
+
 
 
     @Override
