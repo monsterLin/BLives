dd4d0a2736ceb80f5f876c79cf131b0a9edac788
monsterLin <876948462@qq.com>
2016-04-27 12:55:08 +0800

    获取定位信息

--- app/src/main/java/com/monsterlin/blives/navfragment/SquareFragment.java
+++ app/src/main/java/com/monsterlin/blives/navfragment/SquareFragment.java
@@ -11,6 +11,10 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
 import com.baidu.mapapi.map.BaiduMap;
 import com.baidu.mapapi.map.BitmapDescriptor;
 import com.baidu.mapapi.map.BitmapDescriptorFactory;
@@ -47,6 +51,9 @@ public class SquareFragment extends Fragment {
 
     private Button btn_lbs;
 
+    private LocationClient locationClient=null;
+    private static final int UPDATE_TIME=15000;  //更新频率
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -67,15 +74,69 @@ public class SquareFragment extends Fragment {
      * 定位初始化
      */
     private void initLocation() {
+        locationClient =new LocationClient(mContext);
+        //设置定位条件
+        LocationClientOption option = new LocationClientOption();
+        option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy);//设置高精度定位定位模式
+        option.setCoorType("bd09ll");//设置百度经纬度坐标系格式
+        option.setScanSpan(UPDATE_TIME);//设置发起定位请求的间隔时间为1000ms
+        option.setIsNeedAddress(true);//反编译获得具体位置，只有网络定位才可以
+        locationClient.setLocOption(option);  //讲option的初始化信息添加到客户端
+
+        locationClient.registerLocationListener(new BDLocationListener() {
+            @Override
+            public void onReceiveLocation(BDLocation location) {
+                if(location==null){
+                    return;
+                }
+
+                Double Latitude = location.getLatitude() ;//纬度
+                Double Longitude =location.getLongitude(); //精度
+
+                String AddStr  =location.getAddrStr();  //详细地址
+
+                showToast("纬度："+Latitude+"\n"+"精度："+Longitude+"\n"+"详细地址："+AddStr);
+//
+//                LatLng locationLbs = new LatLng(Latitude,Longitude);
+//
+//                BitmapDescriptor bitmap = BitmapDescriptorFactory
+//                        .fromResource(R.drawable.location_arrows);
+//                OverlayOptions option = new MarkerOptions()
+//                        .position(locationLbs)
+//                        .icon(bitmap)
+//                        .title(city);
+//
+//                map.addOverlay(option);
 
 
+
+           }
+        });
     }
+
 
     private void initEvent() {
         btn_lbs.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                    showToast("定位");
+                   // showToast("定位");
+                if(locationClient==null){
+                    return;
+                }if(locationClient.isStarted()){
+                    showToast("定位已关闭");
+                    locationClient.stop();
+                }else{
+                    showToast("正在定位中..");
+                    locationClient.start();
+                    /*
+                     *当所设的整数值大于等于1000（ms）时，定位SDK内部使用定时定位模式。
+                     *调用requestLocation( )后，每隔设定的时间，定位SDK就会进行一次定位。
+                     *如果定位SDK根据定位依据发现位置没有发生变化，就不会发起网络请求，
+                     *返回上一次定位的结果；如果发现位置改变，就进行网络请求进行定位，得到新的定位结果。
+                     *定时定位时，调用一次requestLocation，会定时监听到定位结果。
+                     */
+                    locationClient.requestLocation();
+                }
             }
         });
 
@@ -232,7 +293,11 @@ public class SquareFragment extends Fragment {
     public void onDestroy() {
         super.onDestroy();
         mapView.onDestroy();
-
+        if(locationClient!=null&&locationClient.isStarted())
+        {
+            locationClient.stop();
+            locationClient=null;
+        }
     }
 
     @Override
