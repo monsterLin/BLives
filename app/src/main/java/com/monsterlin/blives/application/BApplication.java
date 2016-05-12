7da03c4b6ff8ea6105ae9463f4043f53494ade4e
monsterLin <876948462@qq.com>
2016-05-12 08:37:51 +0800

    add BugHD

--- app/src/main/java/com/monsterlin/blives/application/BApplication.java
+++ app/src/main/java/com/monsterlin/blives/application/BApplication.java
@@ -4,6 +4,8 @@ import android.app.Application;
 
 import com.baidu.mapapi.SDKInitializer;
 
+import im.fir.sdk.FIR;
+
 /**
  * 应用的Application
  * Created by monsterLin on 2016/4/12.
@@ -13,5 +15,6 @@ public class BApplication  extends Application{
     public void onCreate() {
         super.onCreate();
         SDKInitializer.initialize(getApplicationContext());
+        FIR.init(this);
     }
 }
