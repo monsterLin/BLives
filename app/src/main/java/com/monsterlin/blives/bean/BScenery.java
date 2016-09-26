a8ac230f41b8b9edc8ef5d61b480a4721fa682db
monsterLin <linfanrong235@outlook.com>
2016-09-26 20:53:35 +0800

    因服务器受DDoS扫段攻击，紧急更换校园风光节点

---
+++ app/src/main/java/com/monsterlin/blives/bean/BScenery.java
@@ -0,0 +1,19 @@
+package com.monsterlin.blives.bean;
+
+import cn.bmob.v3.BmobObject;
+
+/**
+ * Created by monsterLin on 2016/9/26.
+ */
+
+public class BScenery extends BmobObject {
+    private String  webUrl ;
+
+    public String getWebUrl() {
+        return webUrl;
+    }
+
+    public void setWebUrl(String webUrl) {
+        this.webUrl = webUrl;
+    }
+}
